769330db38e849e099390c7a4b4a9ccccfe25496
Annie Deborah J <anni3014@global.chainsys.com>
2022-04-28 17:55:54 +0530

    first commit

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxaComponent } from './boxa/boxa.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { HitButtonComponent } from './hit-button/hit-button.component';
+import { CounterAppComponent } from './counter-app/counter-app.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { BookComponent } from './book/book.component';
+import { BookCatlogComponent } from './book-catlog/book-catlog.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookformComponent } from './bookform/bookform.component';
+import { PriceDiscountPipe } from './price-discount.pipe';
+import { HomeComponent } from './home/home.component';
+import { TabComponent } from './tab/tab.component';
+import { NewComponent } from './news/new.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewComponent },
+  { path: 'books', component: BookCatlogComponent },
+  { path: 'addbook', component: BookformComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxaComponent,
+    CustomerFormComponent,
+    HitButtonComponent,
+    CounterAppComponent,
+    EmployeeFormComponent,
+    BookComponent,
+    BookCatlogComponent,
+    BookListComponent,
+    BookformComponent,
+    PriceDiscountPipe,
+    HomeComponent,
+    TabComponent,
+    NewComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ project-eleven/src/app/customer-form/customer-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-form',
+  templateUrl: './customer-form.component.html',
+  styleUrls: ['./customer-form.component.css'],
+})
+export class CustomerFormComponent implements OnInit {
+  //data-model
+  customerModel: any;
+  constructor() {
+    console.log('Customer Loaded');
+  }
+
+  ngOnInit(): void {
+    this.customerModel = {
+      firstName: '',
+      lastName: '',
+      email: 'vkcheeku18@cricket.com',
+    };
+  }
+  saveCustomer(customerFormGroup: any) {
+    if (customerFormGroup.valid) {
+      console.log(customerFormGroup.value);
+    }
+  }
+}
